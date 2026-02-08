862a2ae18e50b07d5ec8d40f419eb8855afc050b
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2026-02-08 19:28:46 +0200

    add contacts validation schemas

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,15 @@
 import Joi from "joi";
 
+// Схема для створення контакту (POST)
 export const createContactSchema = Joi.object({
+    name: Joi.string().min(1).required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().min(1).required(),
+});
 
-})
-
+// Схема для оновлення контакту (PUT)
 export const updateContactSchema = Joi.object({
-
-})
+    name: Joi.string().min(1),
+    email: Joi.string().email(),
+    phone: Joi.string().min(1),
+}).min(1);
