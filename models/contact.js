1f0da5d78914ce8de53a194528c69a05173fab1d
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2026-02-14 23:46:00 +0200

    add contact sequelize model

---
+++ models/contact.js
@@ -0,0 +1,31 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+// Модель контакту для роботи з таблицею contacts
+const Contact = sequelize.define(
+    "contact",
+    {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        favorite: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
+    },
+    {
+        tableName: "contacts",
+        timestamps: false,
+    }
+);
+
+export default Contact;
