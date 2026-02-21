4145db5c06fd6c52751e736a0c56913c3950e82b
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2026-02-21 18:04:04 +0200

    implement user login with jwt authentication

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,8 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.js";
 import { registerSchema } from "../schemas/authSchemas.js";
+import jwt from "jsonwebtoken";
+import { loginSchema } from "../schemas/authSchemas.js";
 
 // Реєстрація користувача
 export const register = async (req, res) => {
@@ -29,6 +31,47 @@ export const register = async (req, res) => {
         user: {
             email: newUser.email,
             subscription: newUser.subscription,
+        },
+    });
+};
+
+// Логін користувача
+export const login = async (req, res) => {
+    const { error } = loginSchema.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({ message: error.message });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+        return res.status(401).json({ message: "Email or password is wrong" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        return res.status(401).json({ message: "Email or password is wrong" });
+    }
+
+    const payload = {
+        id: user.id,
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+    });
+
+    await user.update({ token });
+
+    res.status(200).json({
+        token,
+        user: {
+            email: user.email,
+            subscription: user.subscription,
         },
     });
 };
--- schemas/authSchemas.js
+++ schemas/authSchemas.js
@@ -5,3 +5,9 @@ export const registerSchema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
 });
+
+// Схема логіну користувача
+export const loginSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+});
