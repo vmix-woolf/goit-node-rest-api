40c0f74438d7f3f5fc6678764a0ff2400e2ec7fd
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2026-02-21 17:12:48 +0200

    add user model with relations

---
+++ models/user.js
@@ -0,0 +1,38 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+import Contact from "./contact.js";
+
+// Модель користувача
+const User = sequelize.define(
+    "user",
+    {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        subscription: {
+            type: DataTypes.ENUM,
+            values: ["starter", "pro", "business"],
+            defaultValue: "starter",
+        },
+        token: {
+            type: DataTypes.STRING,
+            defaultValue: null,
+        },
+    },
+    {
+        tableName: "users",
+        timestamps: false,
+    }
+);
+
+// Зв’язок користувач → контакти
+User.hasMany(Contact, { foreignKey: "owner", as: "contacts" });
+Contact.belongsTo(User, { foreignKey: "owner", as: "user" });
+
+export default User;
