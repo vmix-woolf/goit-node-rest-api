1078fb2da86b45d7b8c163eccb7826b6b02a08b3
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2026-03-07 17:52:37 +0200

    Add email verification endpoint GET /auth/verify/:verificationToken

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -171,3 +171,23 @@ export const updateAvatar = async (req, res, next) => {
         next(err);
     }
 };
+
+// Верифікація email користувача
+export const verifyEmail = async (req, res) => {
+    const { verificationToken } = req.params;
+
+    const user = await User.findOne({
+        where: { verificationToken },
+    });
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    await user.update({
+        verify: true,
+        verificationToken: null,
+    });
+
+    res.status(200).json({ message: "Verification successful" });
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../../middlewares/authMiddleware.js";
-import { register, login, logout, getCurrent } from "../../controllers/authControllers.js";
+import { register, login, logout, getCurrent, verifyEmail } from "../../controllers/authControllers.js";
 import { updateSubscription } from "../../controllers/authControllers.js";
 import upload from "../../middlewares/uploadMiddleware.js";
 import { updateAvatar } from "../../controllers/authControllers.js";
@@ -9,6 +9,7 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/verify/:verificationToken", verifyEmail);
 router.post("/logout", authMiddleware, logout);
 router.get("/current", authMiddleware, getCurrent);
 router.patch("/subscription", authMiddleware, updateSubscription);
